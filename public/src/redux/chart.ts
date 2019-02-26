e5257f26d37e203c15c9d1cd1704341ea137b699
sacha barber <sbarber@nephilaadvisors.co.uk>
2019-02-26 21:55:31 +0000

    working on chart

--- public/src/redux/chart.ts
+++ public/src/redux/chart.ts
@@ -1,22 +1,8 @@
 import { ActionWrapper } from '../utils/redux-with-class/actionwrapper';
 import { parseData, parseDate } from '../utils/ChartUtils';
-
 import { tsvParse, csvParse } from "d3-dsv";
 import { timeParse } from "d3-time-format";
 
-//function parseData(parse) {
-//    return function (d) {
-//        d.date = parse(d.date);
-//        d.open = +d.open;
-//        d.high = +d.high;
-//        d.low = +d.low;
-//        d.close = +d.close;
-//        d.volume = +d.volume;
-
-//        return d;
-//    };
-//}
-//const parseDate = timeParse("%Y-%m-%d");
 
 export interface ChartInfoState {
     chartData: any;
